6154a5f3c9c204498474cd821a132e738dae5486
Azat Tagaev <braza@yandex.ru>
2021-02-02 11:31:34 +0600

    Третий урок - усложненный

--- script/script.js
+++ script/script.js
@@ -16,3 +16,32 @@ numMul = String(numMul);
 let twoNum = numMul.substring(0, 2);
 
 console.log(twoNum);
+
+// Lesson03
+
+let lang = 'ru',
+    daysWeek = [
+          ['Понедельник', 'вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+          ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    ];
+
+if (lang === 'ru') {
+    console.log(daysWeek[0]);
+} else {
+    console.log(daysWeek[1]);
+}
+
+switch (lang) {
+    case 'ru':
+        console.log(daysWeek[0]);
+        break;
+    case 'en':
+        console.log(daysWeek[1]);
+}
+
+let command = length === 'en' ? console.log(daysWeek[1]) : console.log(daysWeek[0]);
+
+let namePerson = 'Артем';
+
+namePerson === 'Артем' ? console.log('директор') : console.log('студент');
+namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
